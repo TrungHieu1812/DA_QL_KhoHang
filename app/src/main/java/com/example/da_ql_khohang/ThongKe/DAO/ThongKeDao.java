6ca76dd052eb0ae42cfdb456893983032d09f3e3
DANG SANG <sangldph42693@fpt.edu.vn>
2023-12-10 08:50:21 +0700

    sửa Sign_in

--- app/src/main/java/com/example/da_ql_khohang/ThongKe/DAO/ThongKeDao.java
+++ app/src/main/java/com/example/da_ql_khohang/ThongKe/DAO/ThongKeDao.java
@@ -28,23 +28,7 @@ public class ThongKeDao {
 
     @SuppressLint("Range")
     public List<Top> getSoLuongNhap(){
-        String sqlTop = "select maSP, count(soLuong) as soluong from phieu where loai=0 group by maSP ";
-        List<Top> list = new ArrayList<>();
-        Product_DAO spDao = new Product_DAO(context);
-        Cursor cursor = db.rawQuery(sqlTop,null);
-        while (cursor.moveToNext()){
-            Top top = new Top();
-             Product_model sp = spDao.getProdById(cursor.getInt(cursor.getColumnIndex("maSP")));
-            top.setTen(sp.getTenSP());
-            top.setSoluong(Integer.parseInt(cursor.getString(cursor.getColumnIndex("soluong"))));
-            list.add(top);
-        }
-        return list;
-    }
-
-    @SuppressLint("Range")
-    public List<Top> getSoLuongXuat(){
-        String sqlTop = "select maSP, count(soLuong) as soluong from phieu where loai=1 group by maSP ";
+        String sqlTop = "select maSP, sum(soLuong) as soluong from PHIEU where loai=0 group by maSP order by soluong";
         List<Top> list = new ArrayList<>();
         Product_DAO spDao = new Product_DAO(context);
         Cursor cursor = db.rawQuery(sqlTop,null);
@@ -58,4 +42,51 @@ public class ThongKeDao {
         return list;
     }
 
+    @SuppressLint("Range")
+    public List<Top> getSoLuongXuat(){
+        String sqlTop = "select maSP, sum(soLuong) as soluong from PHIEU where loai=1 group by maSP order by soluong ";
+        List<Top> list = new ArrayList<>();
+        Product_DAO spDao = new Product_DAO(context);
+        Cursor cursor = db.rawQuery(sqlTop,null);
+        while (cursor.moveToNext()){
+            Top top = new Top();
+            Product_model sp = spDao.getProdById(cursor.getInt(cursor.getColumnIndex("maSP")));
+            top.setTen(sp.getTenSP());
+            top.setSoluong(Integer.parseInt(cursor.getString(cursor.getColumnIndex("soluong"))));
+            list.add(top);
+        }
+        return list;
+    }
+
+    @SuppressLint("Range")
+    public List<Top> getTienNhap(){
+        String sqlTop = "select maSP, sum(gia) as gia from CHITIETPHIEU where loai=0 group by maSP order by gia";
+        List<Top> list = new ArrayList<>();
+        Product_DAO spDao = new Product_DAO(context);
+        Cursor cursor = db.rawQuery(sqlTop,null);
+        while (cursor.moveToNext()){
+            Top top = new Top();
+            Product_model sp = spDao.getProdById(cursor.getInt(cursor.getColumnIndex("maSP")));
+            top.setTen(sp.getTenSP());
+            top.setGiaban(Double.parseDouble(cursor.getString(cursor.getColumnIndex("gia"))));
+            list.add(top);
+        }
+        return list;
+    }
+
+    @SuppressLint("Range")
+    public List<Top> getTienXuat(){
+        String sqlTop = "select maSP, sum(gia) as gia from CHITIETPHIEU where loai=1 group by maSP order by gia";
+        List<Top> list = new ArrayList<>();
+        Product_DAO spDao = new Product_DAO(context);
+        Cursor cursor = db.rawQuery(sqlTop,null);
+        while (cursor.moveToNext()){
+            Top top = new Top();
+            Product_model sp = spDao.getProdById(cursor.getInt(cursor.getColumnIndex("maSP")));
+            top.setTen(sp.getTenSP());
+            top.setGiaban(Double.parseDouble(cursor.getString(cursor.getColumnIndex("gia"))));
+            list.add(top);
+        }
+        return list;
+    }
 }
